7f962d7ab92bd81718af5f03d128ef5811019369
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2023-12-25 16:44:17 -0500

    resueltos

---
+++ kata/abbrev_name/main.py
@@ -0,0 +1,23 @@
+#==Generate By touchpy==
+
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+'''
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+    Sam Harris => S.H
+
+    patrick feeney => P.F
+'''
+
+def abbrev_name(name:str):
+    l = list(map(lambda n: n.capitalize()[0], name.split(" ")))
+    return f'{l[0]}.{l[1]}'
+
+if __name__ == "__main__":
+   print(abbrev_name("juan perdomo"))
---
+++ kata/complementary_DNA/main.py
@@ -0,0 +1,39 @@
+#==Generate By touchpy==
+
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+'''
+Adenina -> Timina or Timina -> Adenina
+Guanina -> Citosina or Citosina -> Guanina
+
+
+'''
+
+
+def DNA_strand(dna):
+
+    base_nitrogenadas = {
+            'A':'T', 
+            'T': 'A', # Adenina, Timina
+            'G': 'C', 
+            'C': 'G', # Guanina, Citosina
+        }
+
+    new_dna = ''
+
+    for code in dna:
+        for k, v in base_nitrogenadas.items():
+            if code == k:
+                new_dna += v
+
+    return new_dna
+            
+
+
+
+if __name__ == "__main__":
+    # Casos de prueba
+    print(DNA_strand("AAAA"), "String AAAA is TTTT")
+    print(DNA_strand("ATTGC"), "String ATTGC is ATTGC")
+    print(DNA_strand("GTAT"), "String GTAT is GTAT")
