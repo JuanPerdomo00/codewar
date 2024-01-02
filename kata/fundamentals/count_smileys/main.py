29da60cd6196e6ec6d7bb5b06de02af04c285d29
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2024-01-02 17:45:52 -0500

    resolve

---
+++ kata/fundamentals/count_smileys/main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# === Generate By Touchpy ===
+
+
+'''
+Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+
+        Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+            A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+                Every smiling face must have a smiling mouth that should be marked with either ) or D
+
+                No additional characters are allowed except for those mentioned.
+
+                Valid smiley face examples: :) :D ;-D :~)
+                Invalid smiley faces: ;( :> :} :]
+                Example
+
+                countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+                countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+                countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+
+                Note
+
+                In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.'])']))
+'''
+
+import re
+
+def count_smileys(arr):
+    cout = 0
+    for i in arr:
+        if re.match(r'^[:;][-~]?[)D]$', i):
+            cout += 1
+
+    return cout
+
+
+if __name__ == "__main__":
+    print(count_smileys([':)', ';(', ';}', ':-D']))
+    print(count_smileys([';D', ':-(', ':-)', ';~)']))
+    print(count_smileys([';]', ':[', ';*', ':$', ';-D']))
+
---
+++ kata/fundamentals/mumbling/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# === Generate By Touchpy ===
+
+'''
+This time no story, no theory. The examples below show you how to write function accum:
+    Examples:
+
+    accum("abcd" ->) A-Bb-Ccc-Dddd""
+    accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+    accum"(cwAt") -> "C-Ww-Aaa-Tttt"
+
+    The parameter of accum is a string which includes only letters from a..z and A..Z.
+'''
+
+def accum(s: str):
+    ss  = ''
+    for i, char in enumerate(s):
+        ss += char.capitalize() + char.lower() * i + '-'
+
+    return ss[:-1]
+        
+
+if __name__ == "__main__":
+    print(accum("ZpglnRxqenU"))
