dcec7ebf63a4864a8b3fcc004a2d7e017350c543
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2024-10-27 20:55:42 -0500

    Resolve: add file resolve

---
+++ kata/fundamentals/strings/count/main.py
@@ -0,0 +1,31 @@
+def conut(s: str):
+    # return {} if len(s) == 0 else [(i, x) for i, x in enumerate(s)]
+    if not len(s):
+        return {}
+    
+    new_dict = {}
+    for char in s:
+        if char in new_dict:
+            new_dict[char] += 1
+        else:
+            new_dict[char] = 1
+
+    return new_dict
+
+
+
+'''
+I really didn't know that, it didn't occur to me :*
+'''
+def test(s: str):
+    from collections import Counter
+
+    return Counter(s)
+
+if __name__ == "__main__":
+    # print(conut(""))
+    # print(conut("abae"))
+    # print(conut("aba"))
+    # print(conut("aabb"))
+
+    print(test("abaeeaa"))
