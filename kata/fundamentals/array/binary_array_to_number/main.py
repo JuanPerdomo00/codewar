cd7ae38778ab812a7b30db607e9a358d03e00c26
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2024-11-16 14:32:33 -0500

    Resolve: resolve kata

--- kata/fundamentals/array/binary_array_to_number/main.py
+++ kata/fundamentals/array/binary_array_to_number/main.py
@@ -1,12 +1,11 @@
 def binary_array_to_number(arr):
-  # your code
-  return int(''.join([str(i) for i in arr]), 2)
-
+    # your code
+    return int("".join([str(i) for i in arr]), 2)
 
 
 def main():
-  print(binary_array_to_number([0,0,0,1]))
+    print(binary_array_to_number([0, 0, 0, 1]))
 
 
 if __name__ == "__main__":
-  main()
+    main()
