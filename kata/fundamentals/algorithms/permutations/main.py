3f3f0fef2494622109ffb682521aa167ee07ef69
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2024-11-16 13:33:09 -0500

    Resolve: resolve kata hard xd

---
+++ kata/fundamentals/algorithms/permutations/main.py
@@ -0,0 +1,9 @@
+import itertools
+def permutations(s):
+    # Code Away!
+    return [''.join(p) for p in set(itertools.permutations(s))]
+
+
+print(permutations("a"))
+print(permutations("ab"))
+print(permutations("aabb"))
