ce9c2dc074979a98fba36fae2da4e205c32a6083
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2024-11-24 22:36:41 -0500

    Add: kara resolve python

--- kata/fundamentals/descending_order/main.py
+++ kata/fundamentals/descending_order/main.py
@@ -1,16 +1,15 @@
 # by: jakepys
 
 
-
-def descending_order(num: int) -> int: 
-    return 0 if num <= 0 else int(''.join(sorted(str(num)))[::-1])
+def descending_order(num: int) -> int:
+    return 0 if num <= 0 else int("".join(sorted(str(num)))[::-1])
 
 
 def main():
     print(descending_order(0) == 0)  # 0
     print(descending_order(15) == 51)  # 51
     print(descending_order(123456789) == 987654321)  # 987654321
-                                        # 987654321
+    # 987654321
 
 
 if __name__ == "__main__":
